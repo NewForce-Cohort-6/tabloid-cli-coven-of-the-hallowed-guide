f3cbff4ba84e330c63ea344bbb6341b21cb0217e
shanedbutler <shanedbutler@gmail.com>
2022-10-17 15:54:41 -0400

    Add update SQL client method

--- TabloidCLI/Repositories/PostRepository.cs
+++ TabloidCLI/Repositories/PostRepository.cs
@@ -206,7 +206,30 @@ namespace TabloidCLI.Repositories
 
         public void Update(Post post)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                {
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Post
+                                                SET Title = @title,
+                                                    URL = @url,
+                                                    PublishDate = @publishDateTime,
+                                                    AuthorId = @authorId,
+                                                    BlogId = @blogId
+                                                WHERE id  = @id";
+
+                        cmd.Parameters.AddWithValue("@title", post.Title);
+                        cmd.Parameters.AddWithValue("@url", post.Url);
+                        cmd.Parameters.AddWithValue("@publishDateTime", post.PublishDateTime);
+                        cmd.Parameters.AddWithValue("@authorId", post.Author.Id);
+                        cmd.Parameters.AddWithValue("@blogId", post.Blog.Id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
         }
 
         public void Delete(int id)
