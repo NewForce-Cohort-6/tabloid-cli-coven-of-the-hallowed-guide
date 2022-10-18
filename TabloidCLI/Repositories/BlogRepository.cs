0f77953627721044143fdd38e30b14d8623ab0f7
Cara Netzer <cara.netzer@gmail.com>
2022-10-18 15:56:46 -0400

    add tag to blog

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -149,21 +149,21 @@ namespace TabloidCLI
             }
         }
 
-        //public void InsertTag(Author author, Tag tag)
-        //{
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
-        //            cmd.CommandText = @"INSERT INTO AuthorTag (AuthorId, TagId)
-        //                                               VALUES (@authorId, @tagId)";
-        //            cmd.Parameters.AddWithValue("@authorId", author.Id);
-        //            cmd.Parameters.AddWithValue("@tagId", tag.Id);
-        //            cmd.ExecuteNonQuery();
-        //        }
-        //    }
-        //}
+        public void InsertTag(Blog blog, Tag tag)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO BlogTag (BlogId, TagId)
+                                                       VALUES (@blogId, @tagId)";
+                    cmd.Parameters.AddWithValue("@blogId", blog.Id);
+                    cmd.Parameters.AddWithValue("@tagId", tag.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
 
         //public void DeleteTag(int authorId, int tagId)
         //{
